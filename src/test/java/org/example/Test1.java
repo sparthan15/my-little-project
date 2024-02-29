36e76bb43a3e9b1966e26a2a8c7a43328eeb5c70
Carlos Gamboa Hernandez <carlosgamboah@Carlos-Gamboa.local>
2024-02-28 21:51:07 -0500

    init

---
+++ src/test/java/org/example/Test1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class Test1 {
+
+    @Test
+    void test(){
+        Assertions.assertTrue(false);
+    }
+}
